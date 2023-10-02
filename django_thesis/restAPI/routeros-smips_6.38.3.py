736591a6b1c3ce0b1c4179bfd0d1e718d4a6020c
reyden142 <reydencagata@gmail.com>
2023-10-02 15:56:42 +0800

    MikroTik API

---
+++ django_thesis/restAPI/routeros-smips_6.38.3.py
@@ -0,0 +1,19 @@
+from librouteros import connect
+
+# Define your MikroTik router's IP address, username, and password
+router_ip = '192.168.204.1'
+username = 'admin'
+password = ''
+
+try:
+    # Connect to the router
+    connection = connect(username=username, password=password, host=router_ip)
+
+    # Perform an API query (for example, get system identity)
+    response = connection('/system/identity/print')
+
+    # Iterate through the generator and print each item
+    for item in response:
+        print(item)
+except Exception as e:
+    print(f"An error occurred: {e}")
